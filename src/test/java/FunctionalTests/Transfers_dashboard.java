68d1ebe1bf51a9660150081724a08f087320e7bd
SebastianBandachQA <sebastian.bandach@gmail.com>
2026-04-23 13:10:05 +0200

    Fix app

--- src/test/java/FunctionalTests/Transfers_dashboard.java
+++ src/test/java/FunctionalTests/Transfers_dashboard.java
@@ -5,7 +5,7 @@ import Pages.Enums.Credentials;
 import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 
-public class Transfers extends BaseTestClass {
+public class Transfers_dashboard extends BaseTestClass {
     @Test
     @Description("Przycisk Wyślij przelew jest nieaktywny gdy pole kwoty jest puste")
     public void transferIsInActiveIfTransferFieldIsEmpty(){
@@ -17,5 +17,17 @@ public class Transfers extends BaseTestClass {
                 .quickTransferDesktopButton();
         desktop.veryfi()
                 .insertAmmountModal();
+    }
+    @Test
+    @Description("Przelew z kwotą wyższą niż dostępne saldo jest blokowany z komunikatem")
+    public void transferForAnAmountGreaterThanTheAvailableBalance(){
+        login.act()
+                .login(Credentials.CORRECT_LOGIN)
+                .password(Credentials.PASSWORD)
+                .buttonLoginClick();
+        desktop.act()
+                .quickAmount("50 000")
+                .selectQuickTransferPerson()
+                .quickTransferDesktopButton();
     }
 }
